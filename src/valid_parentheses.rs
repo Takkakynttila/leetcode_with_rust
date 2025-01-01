01b9f5818f95cf2462b7be33d3f114d2b8f3c452
takkakynttila <tatu.possi@gmail.com>
2025-01-01 22:15:39 +0200

    solved valid parentheses

---
+++ src/valid_parentheses.rs
@@ -0,0 +1,93 @@
+#[allow(dead_code)]
+pub fn is_valid(s: String) -> bool {
+    use std::collections::VecDeque;
+    let mut deque = VecDeque::new();
+    for c in s.chars() {
+
+        match c {
+            '(' | '[' | '{' => deque.push_front(c),
+            ')' => {
+                let target = deque.pop_front();
+                
+                if target.is_none() {
+                    return false;
+                }
+
+                if target.unwrap() != '(' {
+                    return false;
+                }
+            }
+            ']' => {
+                let target = deque.pop_front();
+
+                if target.is_none() {
+                    return false;
+                }
+
+                if target.unwrap() != '[' {
+                    return false;
+                }
+            }
+            '}' => {
+                let target = deque.pop_front();
+
+                if target.is_none() {
+                    return false;
+                }
+
+                if target.unwrap() != '{' {
+                    return false;
+                }
+            }
+            _ => {}
+        }
+    }
+    deque.is_empty()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_valid() {
+        let input = "()".to_string();
+        let result = is_valid(input);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_is_valid_mixed() {
+        let input = "([])".to_string();
+        let result = is_valid(input);
+        assert!(result);
+    }
+
+    #[test]
+    fn test_is_invalid_mixed() {
+        let input = "((){[}])".to_string();
+        let result = is_valid(input);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_is_invalid() {
+        let input = "(]".to_string();
+        let result = is_valid(input);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_is_invalid_single_closing() {
+        let input = "]".to_string();
+        let result = is_valid(input);
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_is_invalid_single_opening() {
+        let input = "{".to_string();
+        let result = is_valid(input);
+        assert!(!result);
+    }
+}
